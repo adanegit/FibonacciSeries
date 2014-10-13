ac977b14017f17be3e90eafd8916fbc33f74d1f1
adane21 <adane721@gmail.com>
2014-10-13 00:46:33 -0400

    git commit

--- src/fiboncciseries/FiboncciSeries.java
+++ src/fiboncciseries/FiboncciSeries.java
@@ -5,6 +5,9 @@
  */
 package fiboncciseries;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author adane
@@ -14,8 +17,14 @@ public class FiboncciSeries {
     /**
      * @param args the command line arguments
      */
+    private static Map<Integer, Integer> map;
+
     public static void main(String[] args) {
         // TODO code application logic here
+
+        map = new HashMap<>();
+        System.out.println("Fibonacci of 5 is: " + naiveFib(5));
+        System.out.println("Fibonacci of 5 is: " + memorizedFib(5));
     }
 
     public static int naiveFib(int n) {
@@ -25,6 +34,22 @@ public class FiboncciSeries {
         } else {
             return naiveFib(n - 2) + naiveFib(n - 1);
         }
+
+    }
+
+    public static int memorizedFib(int n) {
+
+        if (map.containsKey(n)) {
+            return map.get(n);
+        } else {
+            if (n == 0 || n == 1) {
+                map.put(n, 1);
+            } else {
+                map.put(n, memorizedFib(n-2) + memorizedFib(n - 1));
+            }
+        }
+        
+        return map.get(n);
 
     }
 
